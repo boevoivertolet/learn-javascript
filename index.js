644fbf29637e6f12db54532b8e8b4a376ea23298
boevoivertolet <bjha78rus@yandex.ru>
2023-06-27 17:45:04 +0300

    typeof null // 'object' lol

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 // alert('«Я JavaScript!»')
 
-function getTodos() {
-	const todos = fetch('https://jsonplaceholder.typicode.com/todos/1')
-		.then((res) => {
-			console.log(res)
-			return res.json()
-		})
-		.then((data) => {
-			console.log(data)
-			return data
-		})
-	console.log(todos)
-}
+// function getTodos() {
+// 	const todos = fetch('https://jsonplaceholder.typicode.com/todos/1')
+// 		.then((res) => {
+// 			console.log(res)
+// 			return res.json()
+// 		})
+// 		.then((data) => {
+// 			console.log(data)
+// 			return data
+// 		})
+// 	console.log(todos)
+// }
 
-async function getAsyncTodos() {
-	try {
-		const todos = await fetch('https://jsonplacehoer.typicode.com/todos/1')
-		console.log('Response :', todos)
-		const json = await todos.json()
-		console.log('JSON :', json)
-		return json
-	} catch (error) {
-		console.error('ERROR >>>> ', error)
-	}
-}
+// async function getAsyncTodos() {
+// 	try {
+// 		const todos = await fetch('https://jsonplacehoer.typicode.com/todos/1')
+// 		console.log('Response :', todos)
+// 		const json = await todos.json()
+// 		console.log('JSON :', json)
+// 		return json
+// 	} catch (error) {
+// 		console.error('ERROR >>>> ', error)
+// 	}
+// }
+let admin
+let name
+name = 'Джон'
+console.log(name)
+admin = name
+alert(admin)
