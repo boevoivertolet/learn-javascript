01a57f9a5ce2440dff72ae04b19072136e6f0be6
boevoivertolet <bjha78rus@yandex.ru>
2023-06-27 14:39:08 +0300

    async await

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // alert('«Я JavaScript!»')
+
+function getTodos() {
+	const todos = fetch('https://jsonplaceholder.typicode.com/todos/1')
+		.then((res) => {
+			console.log(res)
+			return res.json()
+		})
+		.then((data) => {
+			console.log(data)
+			return data
+		})
+	console.log(todos)
+}
+
+async function getAsyncTodos() {
+	try {
+		const todos = await fetch('https://jsonplacehoer.typicode.com/todos/1')
+		console.log('Response :', todos)
+		const json = await todos.json()
+		console.log('JSON :', json)
+		return json
+	} catch (error) {
+		console.error('ERROR >>>> ', error)
+	}
+}
